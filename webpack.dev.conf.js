4034b0d1d3a53dc3d7c21c42409613f2763b279b
Stan Ho <hostiyan.stas@yandex.ru>
2021-03-13 23:28:26 +0300

    Added color cards and types in Colors_Type.pug

--- webpack.dev.conf.js
+++ webpack.dev.conf.js
@@ -9,7 +9,7 @@ const devWebpackConfig = merge(baseWebpackConfig, {
         contentBase: baseWebpackConfig.externals.paths.dist,
         port: 8081,
         overlay: {
-            warnings: true,
+            warnings: false,
             errors: true
         }
     },
